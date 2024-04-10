5578a70fa56ddb0c753ef38d6d922f7d4faf4db8
Arkaitz Cambra <116904224+CAMBRITA@users.noreply.github.com>
2024-04-10 23:24:15 +0200

    Day 1 completed

---
+++ Day 1/Day_1.py
@@ -0,0 +1,28 @@
+'''Advent Calendar'''
+
+with open('C:\Coding\Advent Calendar\datos.txt') as datos:
+    guardaSuma = []
+    suma = 0
+    num = 0
+    sumaTotal = 0
+    guardaNum = 0
+
+    lines = datos.readlines()
+    for line in lines:
+        if line == '\n':
+            num = num + 1
+            guardaSuma.append(suma)
+            if suma == max(guardaSuma):
+                guardaNum = num
+            suma = 0
+        else:
+            suma = suma + int(line)
+
+    print(f"The elf {guardaNum} carries {max(guardaSuma)} calories.")
+
+    guardaSuma.sort()
+
+    for i in range (1,4):
+        sumaTotal = sumaTotal + guardaSuma[len(guardaSuma)-i]
+
+    print(f"The top 3 carries {sumaTotal} calories.")
