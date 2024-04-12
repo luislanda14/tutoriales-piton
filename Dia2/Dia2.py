6e5ed20b024106fb6bf1ffa0fa081fc673842522
Adrian Diverless <adrarrhen@gmail.com>
2024-04-12 20:09:30 +0200

    2 dia a medias

---
+++ Dia2/Dia2.py
@@ -0,0 +1,13 @@
+a = []
+b=0
+with open('test.txt') as f:
+    for x in f:
+        if '\n' in x:
+            a.append(x[:-1])
+        else:
+            a.append(x)
+        if
+        
+        #a[b]
+        #b = b + 1
+    print(a[0][0])
