2896294adc5e59cdf5585b15ed1a43b3d81a1799
Strawberryai <ag6154lk@gmail.com>
2024-04-13 20:01:22 +0200

    Dia 1 terminado

---
+++ day_1/day1_2.py
@@ -0,0 +1,51 @@
+import argparse
+
+def procesar_archivo(input_path):
+    datos = None
+    with open(input_path, 'r') as f:
+        datos = f.readlines()
+    
+    if datos is None:
+        print("No se pudo leer el archivo")
+        return
+
+    return [d.strip() for d in datos]
+
+def guardar_archivo(datos):
+    file_name = "salida.out"
+    with open(file_name, 'w') as f:
+        f.write(str(datos))
+    
+    print(f"Resultados guardados en '{file_name}'.")
+
+def main(input_path):
+    datos = procesar_archivo(input_path)
+    
+
+    # Elfs
+    elfs = []
+    cur_elf = 0
+    for d in datos:        
+        if d == '':
+            elfs.append(cur_elf)
+            cur_elf = 0
+            continue
+
+        cur_elf += int(d)
+
+    # Elf with most calories
+    elfs.sort(reverse=True)
+    res = elfs[0] + elfs[1] + elfs[2]
+    print(res)
+    #guardar_archivo(elfs[0] + elfs[1] + elfs[2])
+    
+
+
+if __name__ == "__main__":
+    #Argumentos de entrada
+    parser = argparse.ArgumentParser(description='Script para Advent of Code Día 1 parte 2')
+    parser.add_argument('input', type=str, help='Archivo de entrada de datos')
+    
+    args = parser.parse_args()
+
+    main(args.input)
