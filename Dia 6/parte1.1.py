32ee8817b68b05732e66489591281e3297de805c
Luis <landa.e.luis@gmail.com>
2024-04-15 13:47:52 +0200

    dai 6

---
+++ Dia 6/parte1.1.py
@@ -0,0 +1,5 @@
+lista = ['a', 'b' , 'c' , 'e' , 'f', 'a', 'c']
+print(len(lista))
+set_lista = set(lista)
+print(set_lista)
+print(len(set_lista))
---
+++ Dia 6/parte1.py
@@ -0,0 +1,31 @@
+def tiene_repetidos(lista_cuatro):
+  
+    n = 0
+    for letrita in lista_cuatro:
+        lista_cuatro[n] = 'aa'
+        if letrita in lista_cuatro:
+            lista_cuatro[n] = letrita
+            return True
+        else:
+            lista_cuatro[n] = letrita      
+        n = n + 1
+
+    return False
+
+with open('input.txt') as f:
+    line = f.read()
+    lista_letras = list(line)
+    lista_cuatro = []
+    for i in range(len(lista_letras)):
+        if len(lista_cuatro) < 4:
+            lista_cuatro.append(lista_letras[i])
+        else:
+            repetidos = tiene_repetidos(lista_cuatro)
+            if repetidos: # si hay numeros repetidos
+                lista_cuatro.pop(0)
+                lista_cuatro.append(lista_letras[i])
+            else: # si no hay repetidos
+                print(i)
+                break
+# 1623
+            
